a7fb25c1a7c538718ef2b99645e5d35736fb8886
M3LYug3N <skrinnik306603@gmail.com>
2025-10-07 07:41:06 +0500

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,11 @@ module.exports = {
   apps: [
     {
       name: "server",
-      script: "/home/rotazap/.bun/bin/bun",
-      args: "run start",
+      script: "npm",
+      args: "run start:prod",
       cwd: "/home/rotazap/rotazap-server",
       env: {
         NODE_ENV: "production",
-        HOST: "0.0.0.0",
-        PORT: "5000",
-        CLIENT_ORIGINS:
-          "https://rotazap.ru,https://www.rotazap.ru,http://45.80.68.160:3000",
       },
     },
   ],
